ecbecdaba0329999100ec19aaf98d3f634efc9e6
codethismachine <jasonalextatum1212@gmail.com>
2017-03-05 11:29:10 -0500

    Add files via upload

---
+++ crystal_palace_management_system/Model/Item.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crystal_palace_management_system.Model;
+
+/**
+ *
+ * @author Jason
+ */
+public class Item {
+    private int itemID;
+    private String itemName; 
+    private double price; 
+    private int quantity;
+    private String description; 
+    private int type; 
+    private String location; 
+    
+    public Item(){
+        
+    }
+    public String getItemName(){
+        return itemName;
+    }
+    public void setItemName(){
+        
+    }
+    public int getItemID(){
+        return itemID;
+    }
+    public void setItemID(){
+        
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(){
+        
+    }
+    public int getQuantity(){
+        return quantity; 
+    }
+    public void setQuantity(){
+        
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(){
+        
+    }
+    
+}
