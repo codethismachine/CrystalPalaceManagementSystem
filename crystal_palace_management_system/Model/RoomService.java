a8e882e22637fae7a4938945d380a8c5e2d428fa
codethismachine <jasonalextatum1212@gmail.com>
2017-04-05 23:21:07 -0400

    Add files via upload

---
+++ crystal_palace_management_system/Model/RoomService.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crystal_palace_management_system.Model;
+
+/**
+ *
+ * @author Jason
+ */
+public class RoomService {
+    private int guest_roomNumber; 
+    private double orderBalance;
+    
+    public RoomService(){
+        
+    }
+    public void modifyOrder(){
+        
+    }
+    public void viewSelection(){
+        
+    }
+    public void addItem(){
+        
+    }
+    public void removeItem(){
+        
+    }
+    public void cancelOrder(){
+        
+    }
+    public void checkCashVisit(){
+        
+    }
+}
